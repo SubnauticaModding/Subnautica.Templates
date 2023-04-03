ee61a97e3cf904a139f3ae8150b30d92c24abe8f
Metious <71298690+Metious@users.noreply.github.com>
2023-04-04 02:03:07 +0430

    Initial commit

---
+++ Subnautica.Templates/templates/BZModding.Default/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace BZModding.Default;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+    
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    private void Awake()
+    {
+        // plugin startup logic
+        Logger = base.Logger;
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
